bfafde991687da308d8a4df95b2a864ad4bb8609
Timur Çelikel <me@timurcelikel.com>
2016-04-16 20:03:47 +0200

    print display

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 class mixer {
     constructor(ingredients) {
         this._mixerDisplay = document.getElementById('mixerDisplay');
+        this._mixerPrintScreen = document.getElementById('mixerPrintScreen');
+
         this.originalIngredients = ingredients;
-        this.addedIngredients = [[]];
-        this.currentIngredient = this._getCurrentIngredient();
+        this.addedIngredients = [];
         this._addEventHandlers();
-        this._mixerDisplay.innerHTML = this.currentIngredient;
+        this._mixerDisplay.innerHTML = this.originalIngredients[0][0];
         this._currentlyChanging = false;
+        this._pointers = {
+            row: 0,
+            ingredient: 0
+        };
     }
 
     addIngredient(ingredient, isAdded) {
@@ -26,26 +31,30 @@ class mixer {
     }
 
     _addCurrentIngredient(add) {
+
         const lastIngedient = this.currentIngredient;
-        this.currentIngredient = this._getCurrentIngredient(); 
+        this.currentIngredient = this._getCurrentIngredient();
 
-        if (this.currentIngredient) {
-            this.addedIngredients[this.addedIngredients.length - 1].push(
-                {
-                    name: lastIngedient,
-                    value: add
-                }
-            )
-
-            if (!this.originalIngredients[0].length) {
-                this.originalIngredients.shift();
-                this.addedIngredients.push([]);   
-            }
-        } else {
-            this._print();
+        if (!this.addedIngredients[this._pointers.row]) {
+            this.addedIngredients.push([]);
         }
 
-        this._displayIngredient(lastIngedient, this.currentIngredient, add);
+        this.addedIngredients[this._pointers.row][this._pointers.ingredient] =
+            {
+                name: this.currentIngredient,
+                value: add
+            };
+
+        const nextPointer = this._incrementPointers(this._pointers)
+
+        if (nextPointer) {
+            this._displayIngredient(this.currentIngredient, this.originalIngredients[nextPointer.row][nextPointer.ingredient], add);
+        } else {
+            this._displayIngredient(this.currentIngredient, undefined, add);
+            this._print();
+        }
+        this._pointers = this._incrementPointers(this._pointers);
+
     }
 
     _displayIngredient(last, current, isAdded) {
@@ -66,11 +75,41 @@ class mixer {
     }
 
     _getCurrentIngredient() {
-        return this.originalIngredients[0] ? this.originalIngredients[0].shift() : undefined;
+        let ingredient = this.originalIngredients[this._pointers.row][this._pointers.ingredient];
+        return ingredient;
+    }
+
+    _incrementPointers(pointer) {
+        if (this.originalIngredients[pointer.row][pointer.ingredient + 1]){
+            return {
+                row: pointer.row,
+                ingredient: pointer.ingredient + 1
+            }
+        }
+        if (this.originalIngredients[pointer.row + 1]) {
+            return {
+                row: pointer.row + 1, 
+                ingredient: 0
+            }
+        }
     }
 
     _print() {
-        console.log('printing...')
+        let printOutput = '';
+
+        for (let line of this.addedIngredients) {
+            printOutput += '<p>';
+
+            for(let ingredient of line) {
+                printOutput += '<span class="mixer-ingredient--'
+                printOutput += ingredient.value ? 'added' : 'rejected';
+                printOutput += '">' + ingredient.name + '</span>&nbsp;'
+            }
+
+            printOutput += '</p>';
+        }
+
+        this._mixerPrintScreen.innerHTML = printOutput;
     }
 }
 
