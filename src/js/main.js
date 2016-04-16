63ea800e19d28ca21f58a493b6c9d26996045f1f
Timur Çelikel <me@timurcelikel.com>
2016-04-16 15:58:43 +0200

    basic mixer structure, set ingedients

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,44 @@
-console.log('hello, world.');
+class mixer {
+    constructor(ingredients) {
+        console.log('foo', ingredients);
+        this.addedIngredients = [];
+    }
+
+    addIngredient(ingredient, isAdded) {
+        this.addedIngredients.push(ingredient, isAdded);
+    }
+}
+
+const ingredients = {
+    schnapps: [
+        'rum',
+        'vodka',
+        'kräuter',
+        'tequila',
+        'korn',
+        '43',
+        'zubrovka',
+        'martini',
+        'gin'
+    ],
+    soft: [
+        'milch',
+        'orangensaft',
+        'tonic',
+        'cola',
+        'mate',
+        'tomatensaft'
+    ],
+    decoration: [
+        'limetten',
+        'rohrzucker',
+        'zitronen',
+        'sellerie',
+        'tabasco',
+        'pfeffer',
+        'gurke'
+    ]
+}
+
+const voMix = new mixer(ingredients);
+
